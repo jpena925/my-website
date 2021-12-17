f976c073c28f0db189fd2e58065bc42730cdf02e
Jack Pena <penajackc@gmail.com>
2021-12-17 09:32:34 -0600

    mouseover, css update

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const h2 = document.createElement("h2");
 h2.textContent = "How to stop being cheugy";
 document.querySelector("body").appendChild(h2);
 
+const test = document.getElementById('pic');
+test.addEventListener("mouseenter", function(event){
+    alert("Don't click me!");
+});
+
